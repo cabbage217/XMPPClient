dc820bd0134f4211b4fe450c432ae07ae1d5797e
cabbage217@gmial.com <cabbage217@gmail.com>
2012-05-25 20:58:55 +0800

    Initial Commit

---
+++ XMPPClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XMPPClient
+//
+//  Created by cabbage on 5/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ XMPPClient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XMPPClient
+//
+//  Created by cabbage on 5/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
